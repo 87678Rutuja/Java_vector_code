a2b15b2dbdfe46f1e0882b0cce66bed7668e0a7a
87678Rutuja <132226247+87678Rutuja@users.noreply.github.com>
2023-08-11 17:00:33 +0530

    Add files via upload

---
+++ vector.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class VectorDemo{
+	public static void main(String[]ar){
+		Vector v = new Vector();
+
+		v.addElement(10);
+		  v.addElement(20);
+		    v.addElement(30);
+		      v.addElement(40);
+
+
+		        System.out.println(getSize());
+		      System.out.println(v);
+	}
+}
